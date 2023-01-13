2921f5fe90cdb05e6d2802be641fb90f175ed11e
Andrii Grechyn <gre4ya@icloud.com>
2023-01-12 21:34:33 -0600

    added new lines of code

--- src/TestCase4.java
+++ src/TestCase4.java
@@ -1,5 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import utils.Driver;
+
 public class TestCase4 {
     public static void main(String[] args) {
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://automationexercise.com/");
 
+        WebElement copyrightFooter = driver.findElement(By.cssSelector(".footer-bottom p"));
+        System.out.println(copyrightFooter.isDisplayed() ?
+                "Copyright footer IS Displayed" : "Copyright footer IS NOT Displayed");
+
+        System.out.println("Copyright footer text is - \"" + copyrightFooter.getText() + "\"");
+
+        System.out.println(copyrightFooter.getText().equals("Copyright © 2021 All rights reserved") ?
+                "Copyright footer Verification PASSED" : "Copyright footer Verification FAILED");
+
+        Driver.quitDriver();
     }
 }
--- src/TestCase5.java
+++ src/TestCase5.java
@@ -1,2 +1,28 @@
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import utils.Driver;
+import utils.Waiter;
+
 public class TestCase5 {
+    public static void main(String[] args) {
+        WebDriver driver = Driver.getDriver();
+        driver.get("https://automationexercise.com/");
+        Waiter.pause(2);
+
+        driver.findElement(By.xpath("(//ul/li)[5]/a")).click();
+        Waiter.pause(2);
+
+        Alert alert = driver.switchTo().alert();
+        alert.dismiss();
+
+
+
+
+        System.out.println(driver.getCurrentUrl().equals("https://automationexercise.com/test_cases") ?
+                "Validation of navigation PASSED" : "Validation of navigation FAILED");
+
+
+       // Driver.quitDriver();
+    }
 }
